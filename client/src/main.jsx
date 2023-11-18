8307377fb9ca9f98d727210e6636d171d687e097
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-11-18 10:19:38 -0500

    restructure and refactor routes strategy

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -11,9 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
+        <App />
       </PersistGate>
     </Provider>
   </React.StrictMode>
