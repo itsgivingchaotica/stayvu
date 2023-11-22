ae579f26775a5ed479c6ef38cadaf7311cc3296c
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-11-22 01:32:50 -0500

    accomodate filtered search

--- client/src/pages/Listings.jsx
+++ client/src/pages/Listings.jsx
@@ -1,14 +1,27 @@
 // Listings.js
-import React from 'react';
-import ListingsContainer from '../containers/ListingsContainer';
+import React, { useEffect } from "react";
+import ListingsContainer from "../containers/ListingsContainer";
+import { useSelector } from "react-redux";
 
 const Listings = ({ data }) => {
-  return (
+  const filteredListings = useSelector(
+    (state) => state.listings?.filteredListings
+  );
+
+  useEffect(() => {
+    // This effect will run whenever filteredListings changes
+    console.log("filteredListings changed:", filteredListings);
+  }, [filteredListings]);
+
+  return filteredListings.length > 0 ? (
+    <div className="h-screen mt-6 mb-6 mx-6">
+      <ListingsContainer data={filteredListings} />
+    </div>
+  ) : (
     <div className="h-screen mt-6 mb-6 mx-6">
       <ListingsContainer data={data} />
     </div>
   );
 };
-
 
 export default Listings;
