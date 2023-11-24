62365ac9fbe16d91228c9044662deaccc39d0a2b
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-11-24 17:26:09 -0500

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,17 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3001",
+export default ({ mode }) => {
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: mode === "production" ? "https://stayvue.up.railway.app" : "http://localhost:3001",
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
       },
     },
-  },
-});
+  });
+};
