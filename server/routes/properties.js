a7ca3e9dde20c7263170b0cf49ca9d0450803b27
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-11-29 21:46:54 -0500

    add images routes

--- server/routes/properties.js
+++ server/routes/properties.js
@@ -1,11 +1,31 @@
 import express from "express";
 
 import PropertiesController from "../controllers/properties.js";
+import { upload, uploadMultiple } from "../utils/multer.js";
 
 const router = express.Router();
 
 router.get("/:userId", PropertiesController.getUserProperties);
 router.get("/view/:propertyId", PropertiesController.getPropertyById);
-router.post("/new/:userId", PropertiesController.postNewProperty);
+router.get(
+  "/images/retrieve/:propertyId",
+  PropertiesController.getPropertyImages
+);
+router.post("/new/:hostId", PropertiesController.postNewProperty);
+router.post(
+  "/images/single/:propertyId",
+  (req, res, next) => {
+    upload(req, res, next);
+  },
+  PropertiesController.postNewPropertySingleImage
+);
+router.post(
+  "/images/multiple/:propertyId",
+  (req, res, next) => {
+    uploadMultiple(req, res, next);
+  },
+  PropertiesController.postNewPropertyImages
+);
+router.delete("/delete/:propertyId", PropertiesController.deletePropertyById);
 
 export default router;
