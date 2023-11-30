b064af7d02f1eb4d93e0f5ad4e129b187aa1b3fa
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-11-29 21:45:38 -0500

    listing images props fix:

--- client/src/components/ListingCard.jsx
+++ client/src/components/ListingCard.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ListingCarousel from "./ListingCarousel";
 import { BsFillStarFill } from "react-icons/bs";
 import { format, isSameMonth } from "date-fns";
+import axios from "axios";
 
 const ListingCard = ({ listing }) => {
+  const [listingImages, setListingImages] = useState([]);
+  const API_URL =
+    process.env.NODE_ENV === "production"
+      ? import.meta.env.VITE_SERVER_URL
+      : "http://localhost:3001";
+
   if (!listing || !listing.availability) {
-    console.error('Invalid or missing listing data:', listing);
-    return null; // or handle it in another way
+    console.error("Invalid or missing listing data:", listing);
+    return null; 
   }
 
   const formattedAvailability = listing.availability.map((item) => {
@@ -14,8 +21,8 @@ const ListingCard = ({ listing }) => {
     const endDate = new Date(item.end_availability);
 
     if (!startDate || !endDate) {
-      console.error('Invalid date range:', item);
-      return null; // or handle it in another way
+      console.error("Invalid date range:", item);
+      return null; 
     }
 
     const startMonth = format(startDate, "MMM");
@@ -28,19 +35,34 @@ const ListingCard = ({ listing }) => {
     } else {
       formattedDate = `${startMonth} ${startDate.getDate()} - ${endMonth} ${endDate.getDate()}`;
     }
-
     return formattedDate;
   });
 
   if (formattedAvailability.includes(null)) {
-    console.error('Invalid date range in availability:', listing.availability);
-    return null; // or handle it in another way
+    console.error("Invalid date range in availability:", listing.availability);
+    return null;
   }
+
+  useEffect(() => {
+    const fetchListingImages = async () => {
+      try {
+        const res = await axios.get(
+          `${API_URL}/api/properties/images/retrieve/${listing.property_id}`,
+          { withCredentials: true }
+        );
+        setListingImages(res.data);
+      } catch (error) {
+        console.error("Error fetching single property images", error);
+        throw error;
+      }
+    };
+    fetchListingImages(listing?.property_id);
+  }, []);
 
   return (
     <div className="flexbg-white w-full max-w-sm border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       <a href="#">
-        <ListingCarousel images={listing.images} />
+        <ListingCarousel images={listingImages} />
       </a>
       <div className="px-5 py-2 pb-5">
         <div className="flex justify-between">
